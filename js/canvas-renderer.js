884ae008dfce50309ac1ecd7262a74d137397dbc
Max Koloskov <koloskov.maxim@mail.ru>
2016-06-04 01:16:42 +0300

    selected block highlight improved

--- js/canvas-renderer.js
+++ js/canvas-renderer.js
@@ -1,14 +1,16 @@
 function CanvasRenderer(opts) {
     this.fieldSize = opts.fieldSize;
 
+    this.spacing = opts.spacing || 2;
+
     this.cubesColors = opts.cubesColors;
 
     this.elementId = opts.elementId || 'game-field';
     this.$el = document.getElementById(this.elementId);
     this.ctx = this.$el.getContext('2d');
 
-    this.cubeWidth = this.$el.width / this.fieldSize[0];
-    this.cubeHeight = this.$el.height / this.fieldSize[1];
+    this.cubeWidth = (this.$el.width - (this.fieldSize[0] - 1) * this.spacing) / this.fieldSize[0];
+    this.cubeHeight = (this.$el.height - (this.fieldSize[1] - 1) * this.spacing) / this.fieldSize[1];
 }
 
 CanvasRenderer.prototype.render = function(field) {
@@ -17,28 +19,30 @@ CanvasRenderer.prototype.render = function(field) {
     field.forEachCube(function (cube) {
         if (cube) {
             self.ctx.save();
-            if (cube.selected) self.ctx.globalAlpha = 0.7;
-            self._drawCube(cube.x, cube.y, cube.color);
+            self._drawCube(cube.x, cube.y, cube.color, cube.selected);
             self.ctx.restore();
         }
     });
 };
 
-CanvasRenderer.prototype._drawCube = function (x, y, color) {
+CanvasRenderer.prototype._drawCube = function (x, y, color, selected) {
     this.ctx.fillStyle = color;
-    this.ctx.strokeStyle = '#cdc0b0';
-    this.ctx.lineWidth = 2;
+
+    if (selected) {
+        this.ctx.shadowBlur = 15;
+        this.ctx.shadowColor = '#dfd8d8';
+        this.ctx.globalAlpha = 0.6;
+    }
 
     this.ctx.beginPath();
-    this.ctx.rect(this.cubeWidth*x, this.cubeHeight*y, this.cubeWidth, this.cubeHeight);
+    this.ctx.rect((this.cubeWidth + this.spacing)*x, (this.cubeHeight + this.spacing)*y, this.cubeWidth, this.cubeHeight);
     this.ctx.fill();
-    this.ctx.stroke();
     this.ctx.closePath();
 };
 
 CanvasRenderer.prototype.screenToCell = function (posx, posy) {
-    var x = Math.ceil(posx/this.cubeWidth);
-    var y = Math.ceil(posy/this.cubeHeight);
+    var x = Math.ceil(posx/(this.cubeWidth + this.spacing));
+    var y = Math.ceil(posy/(this.cubeHeight + this.spacing));
 
     return {x: x-1, y: y-1};
 };
